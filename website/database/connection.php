7f4a890b2d57364e881670fad07a5571734748f7
jazzchipc <josealeixo.pc@outlook.com>
2016-11-30 17:38:04 +0000

    update

--- website/database/connection.php
+++ website/database/connection.php
@@ -1,9 +1,11 @@
+<!-- Establish connection to database -->
+
 <?php
   try {
-     $dbh = new PDO('sqlite:restaurant.db');
-     $dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC); 
-     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+     $dbh = new PDO('sqlite:sqlite/db');
+     $dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);  # o array é indexado pelo NOME da coluna
+     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);       # permite que um erro seja detetado
   } catch (PDOException $e) {
-     die($e->getMessage());
+     die($e->getMessage()); # die == terminar processo
   }
 ?>
