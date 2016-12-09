9a4e16839db6376ff747f789af49f64c70c81c68
Catarina Correia <aniratac.correia@gmail.com>
2016-12-09 12:42:20 +0000

    Changed email type

--- website/registration_user.php
+++ website/registration_user.php
@@ -5,7 +5,7 @@
 <form action="database/add_user.php" method="post">
 
     <label>Username<input name="username" type="text" required></label>
-    <label>Email<input name="email" type="text" required></label>
+    <label>Email<input name="email" type="email" required></label>
     <label>Password<input name="password" type="password" required></label>
     <input type="submit" value="Register">
 
