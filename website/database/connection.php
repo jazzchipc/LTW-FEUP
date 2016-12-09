c8e1977b186c83b19900c08fc2f3c53afd67c7a5
jazzchipc <josealeixo.pc@outlook.com>
2016-12-09 13:05:27 +0000

    Added new line to make a PDO connection

--- website/database/connection.php
+++ website/database/connection.php
@@ -2,7 +2,9 @@
 
 <?php
   try {
-    $dbh = new PDO('sqlite:database/sqlite/db');  # absolute path from webroot. Using '/' does not work, so no need for that.
+    $dbh = new PDO('sqlite:'.$_SERVER['DOCUMENT_ROOT'].'/database/sqlite/db');
+    #OR
+    #$dbh = new PDO('sqlite:database/sqlite/db');  # absolute path from webroot. Using '/' does not work, so no need for that.
     #OR
     #$dbh = new PDO('sqlite:'.__DIR__.'/sqlite/db');  # .__DIR__. is the directory of the current file
     $dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);  # o array é indexado pelo NOME da coluna
