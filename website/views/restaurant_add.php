55a680ce8ad93355099f5a279218c4ede63122b5
jazzchipc <josealeixo.pc@outlook.com>
2016-12-11 17:29:25 +0000

    Added spinner for close and opneing time of restaurant.

---
+++ website/views/restaurant_add.php
@@ -0,0 +1,104 @@
+<?php
+
+    include('../templates/header.php'); 
+
+?>
+
+
+
+<script>
+// Hours spinner OPEN
+  $( function() {
+    $( "#spinner_hours_open" ).spinner({
+      spin: function( event, ui ) {
+        if ( ui.value > 24 ) {
+          $( this ).spinner( "value", 0 );
+          return false;
+        } else if ( ui.value < 0 ) {
+          $( this ).spinner( "value", 24 );
+          return false;
+        }
+      }
+    }).val(8);
+  } );
+
+// Minutes spinner OPEN
+    $( function() {
+        $( "#spinner_minutes_open" ).spinner({
+            spin: function( event, ui ) {
+            if ( ui.value > 60 ) {
+                $( this ).spinner( "value", 0 );
+                return false;
+            } else if ( ui.value < 0 ) {
+                $( this ).spinner( "value", 60 );
+                return false;
+            }
+            }
+        }).val(00);;
+    });
+
+// Hours spinner CLOSE
+  $( function() {
+    $( "#spinner_hours_close" ).spinner({
+      spin: function( event, ui ) {
+        if ( ui.value > 24 ) {
+          $( this ).spinner( "value", 0 );
+          return false;
+        } else if ( ui.value < 0 ) {
+          $( this ).spinner( "value", 24 );
+          return false;
+        }
+      }
+    }).val(20);
+  } );
+
+// Minutes spinner CLOSE
+    $( function() {
+        $( "#spinner_minutes_close" ).spinner({
+            spin: function( event, ui ) {
+            if ( ui.value > 60 ) {
+                $( this ).spinner( "value", 0 );
+                return false;
+            } else if ( ui.value < 0 ) {
+                $( this ).spinner( "value", 60 );
+                return false;
+            }
+            }
+        }).val(00);;
+    });
+
+</script>
+
+<form action ="" method ="post" id ="restaurant_form" enctype="multipart/form-data">
+
+    <div id="restaurant_information">
+        <div><label>Name</label>
+        <input type="text" name="restaurant_name" required></input></div>
+
+        <div><label>Description</label>
+        <textarea name="restaurant_description" required></textarea></div>
+
+        <div><label>Image</label>
+        <input type="file" name="restaurant_image"></input></div>
+
+        <div>
+            <label for="spinner">Open time:</label>
+            <input class="spinner_jquery" id="spinner_hours_open" name="hours">h<input id="spinner_minutes_open"  class="spinner_jquery" name="minutes">min
+            <label for="spinner">Close time:</label>
+            <input class="spinner_jquery" id="spinner_hours_close" name="hours">h<input id="spinner_minutes_close"  class="spinner_jquery" name="minutes">min
+        </div>
+        
+    </div>
+
+    <div>
+        <input type="submit" value="Add Restaurant">
+    </div>
+
+</form>
+
+
+<?php
+
+    include('../templates/footer.php');
+
+?>
--- website/views/restaurants_index.php
+++ website/views/restaurants_index.php
@@ -36,7 +36,7 @@ try {
 }
 ?>
 
-<a href="/actions/add_restaurant.php">Add restaurant</a>
+<a href="/views/<restaurant_add></restaurant_add>.php">Add restaurant</a>
 
 <?php
   include('../templates/footer.php');
