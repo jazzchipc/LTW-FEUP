5f4ad167bb74214d2b3354d399db6c00b08a059d
Catarina Correia <aniratac.correia@gmail.com>
2016-12-13 16:40:28 +0000

    changed colors and font

--- website/templates/header.php
+++ website/templates/header.php
@@ -9,6 +9,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+   <link href="https://fonts.googleapis.com/css?family=Amatic+SC|Poiret+One|Pontano+Sans" rel="stylesheet">
     <link rel="stylesheet" href="/resources/css/reset.css">
     <link rel="stylesheet" href="/resources/css/jquery-ui.theme.css">
     <link rel="stylesheet" href="/resources/css/jquery-ui.min.css">
